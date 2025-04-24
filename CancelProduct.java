853c88f87fae1bbda3fbf8fc6fb93fa88fefd71e
Thai Hoang Dinh <thai.hoang.dinh@pirago.work>
2025-04-24 10:42:05 +0700

    Update testcase CancelProdcut

--- CancelProduct.java
+++ CancelProduct.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class CancelProduct extends BaseTest {
-	private final By product = By.xpath("//a[@data-id='329429']");
-	private final By addBtn = By.xpath("/html/body/section/div[2]/div[2]/div[4]/div[2]/div[6]");
-	private final By boxBtn = By.xpath("//a[@class='btn-buynow white']");
+	private final By product = By.xpath("/html/body/div[7]/div/div[1]/div/div[2]/div/div[3]/div/div/div/div[1]/div[1]/div");
+	private final By addToCartBtn = By.cssSelector("a.btn-buynow");
 	private final By city = By.xpath("//a[contains(text(),'Hà Nội')]");
 	private final By district = By.xpath("//a[contains(text(),'Quận/Huyện')]");
 	private final By districtChild = By.xpath("//a[contains(text(),'Huyện Thanh Trì')]");
@@ -24,15 +23,17 @@ public class CancelProduct extends BaseTest {
 	private final By orderBtn = By.xpath("//button[contains(text(),'Đặt hàng')]");
 	private final By messageSuccess = By.xpath("//div[@class='alertsuccess-new']");
 	private final By cancelBtn = By.xpath("//a[contains(@class, 'text-[#DD1C1A]') and text()='Hủy']");
-	private final By reason = By.xpath("//label[@for='checkBox_5399']//span[@class='cbx-crmsurvey svelte-qjufql']");
-	private final By cancelConfirmBtn = By.xpath("//button[@class='button-send-report-survey' and text()='XÁC NHẬN']");
-
+	private final By reason = By.xpath("//span[text()='Vấn đề khác']");
+	private final By cancelConfirmBtn = By.xpath("//button[@id='crmbtnsubmitsurveyError']");
+																																																																																																								
 	@Test(priority = 1)
 	public void writeInformation() {
 		click(product);
 		pause(1000);
-		click(addBtn);
-		pause(500);
+		scrollToElement(addToCartBtn);
+		pause(1500);
+		clickAddToCart();
+		pause(2000);
 		click(city);
 		pause(500);
 		click(district);
@@ -46,26 +47,22 @@ public class CancelProduct extends BaseTest {
 		input(location, "Việt Yên");
 		pause(500);
 		click(confirmBtn);
-		pause(500);
+		pause(2000);
+//		clickAddToCart();
+		click(cardView);
+		pause(2000);
 		System.out.println("Đã nhập đầy đủ thông tin");
 	}
 
 	@Test(priority = 2)
-	public void addProductToCard() {
-		click(boxBtn);
-		pause(500);
-		click(cardView);
-		pause(500);
-	}
-
-	@Test(priority = 3)
 	public void inputInformation() {
 		click(addInfo);
+		pause(1000);
 		click(genderInfo);
 		pause(1000);
-		input(nameInfo, "Nguyen Trung Tran");
+		input(nameInfo, "Hang Hoang Tran");
 		pause(1000);
-		input(phoneInfo, "0981235344");
+		input(phoneInfo, "0934567657");
 		pause(1000);
 		click(confirmSubmitBtn);
 		pause(1000);
@@ -73,19 +70,19 @@ public class CancelProduct extends BaseTest {
 		pause(1000);
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 3)
 	public void checkOrderSuccess() {
 		click(paymentMethod);
 		pause(2000);
 		click(orderBtn);
-		pause(2000);
+		pause(20000);
 		orderSuccess(messageSuccess, "ĐẶT HÀNG THÀNH CÔNG");
-		click(cancelBtn);
-		pause(1000);
+		cancelProduct();
+		pause(20000);
 		click(reason);
-		pause(2000);
-		click(cancelConfirmBtn);
 		pause(1000);
-		System.out.println("HỦY ĐƠN HÀNG THÀNH CÔNG.");
+		click(cancelConfirmBtn);
+		pause(10000);
+		System.out.println("HỦY ĐƠN HÀNG THÀNH CÔNG");
 	}
 }
